617c8b872b13ec7bfc54880e65316892c8604963
Austin Nevins <amnevins@gmail.com>
2018-02-07 16:03:46 -0800

    second

--- Project1/lambda.js
+++ Project1/lambda.js
@@ -1,4 +1,5 @@
 let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
 
@@ -10,6 +11,24 @@ exports.handler = function (event, context, callback) {
 			console.log('oh noeees', err, err.stack);
 			callback(err, err.stack)
 		} else {
+			sns.publish({
+				Message: 'A new doughnut of type ',
+				Subject: 'Doughnut Added!',
+				MessageAttributes: {
+					'type': {
+						DataType: 'String',
+						StringValue: 'event.doughnut.type'
+					},
+				},
+				MessageStructure: 'String',
+				TopicArn: 'arn:aws:sns:us-east-1:131603044023:doughnut_shop'
+			}).promise()
+				.then(data => {
+					console.log('yay message sent ', data);
+				})
+				.catch(err => {
+					console.log('oh noooesss no message sent');
+				});
 			console.log(`yay doughnut of ${event.doughnut.type} has been stored in inventory`)
 		}
 	});
