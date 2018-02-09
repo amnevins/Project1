11f0cf6fa7d84f1a9c4426f28ffacddececb4519
Austin Nevins <amnevins@gmail.com>
2018-02-08 17:59:02 -0800

    postTest

--- Project1/postDoughnut.js
+++ Project1/postDoughnut.js
@@ -1,6 +1,39 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+	console.log(event);
+	console.log(event.body);
+	ddb.put({
+		TableName: 'DoughnutInventory',
+		Item: { 'type': dataEvent }
+	}, function (err, data) {
+		if (err) {
+			console.log('error putting to table ', err, err.stack)
+			callback(errorCallback);
+		} else {
+			console.log(data);
+			callback(null, successCallback);
+		}
+	});
 
-
-	callback(null,'Successfully executed');
+	const errorCallback = {
+		"isBase64Encoded": 1,
+		"statusCode": 500,
+		"headers": {
+			"content-type": "application/json"
+		},
+		"body": {
+			"data": `oops we got problems`
+		}
+	}
+	const successCallback = {
+		"isBase64Encoded": 1,
+		"statusCode": 200,
+		"headers": {
+			"content-type": "application/json"
+		},
+		"body": {
+			"data": `${dataEvent.doughnut.type} recorded`
+		}
+	}
 }
