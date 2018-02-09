7d76787a8ea1357e185f779aa3ead2643ddbdf8c
Austin Nevins <amnevins@gmail.com>
2018-02-08 21:10:29 -0800

    2

--- Project1/postDoughnut.js
+++ Project1/postDoughnut.js
@@ -2,7 +2,7 @@ let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
 	console.log(event);
-	console.log(event.body);
+	// console.log(event.body);
 
 	const errorCallback = {
 		"isBase64Encoded": 1,
@@ -37,5 +37,5 @@ exports.handler = function (event, context, callback) {
 	// 	}
 	// });
 	
-	callback(null, successCallback);
+	return callback(null, successCallback);
 }
