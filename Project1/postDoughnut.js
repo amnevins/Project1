96c0d9e3e0f2fb658224affc1a3a6b0926949df4
Austin Nevins <amnevins@gmail.com>
2018-02-09 15:16:29 -0800

    barf23

--- Project1/postDoughnut.js
+++ Project1/postDoughnut.js
@@ -18,9 +18,9 @@ exports.handler = function (event, context, callback) {
 		"headers": {
 			"content-type": "application/json"
 		},
-		"body": {
+		"body": JSON.stringify({
 			'data': 'doughnut recorded'
-		)
+		})
 	}
 	ddb.put({
 		TableName: 'DoughnutInventory',
